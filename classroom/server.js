36f2eb6891cc4c37fe48f1317d894a8ac18d9998
bhushan-rajput18 <patilbhushan0222@gmail.com>
2026-07-11 19:44:48 +0530

    express-session, storing & accessing info and connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -1,44 +1,72 @@
 import express from 'express';
 const app = express();
-import users from "./routes/user.js"
-import posts from "./routes/post.js"
-import cookieParser from 'cookie-parser';
+// import users from "./routes/user.js"
+// import posts from "./routes/post.js"
+import session from 'express-session';
+import flash from 'connect-flash';
+import ejsMate from 'ejs-mate';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-app.use(cookieParser("secretcode"))
 
-app.get("/getsignedcookies", (req, res) => {
-    res.cookie("madeIn", "India", {signed: true})
-    res.send("signed cookie sent ")
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/users", users);
+// app.use("/posts", posts);
+
+const sessionOptions = {
+    secret: "mysupersecret",
+    resave: false, 
+    saveUninitialized: true
+}
+
+app.use(session(sessionOptions))
+app.use(flash())
+
+//middleware
+app.use((req, res, next) => {
+    res.locals.messages = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 })
 
-app.get("/varify", (req,res) => {
-    console.log(req.signedCookies);
-    res.send("got the signed cookies ");
+app.get("/register", (req, res) => {
+    let { name = "anonymous"} = req.query;
+    req.session.name = name;
+
+    if(name === "anonymous") {
+        req.flash("error", "user is not registered")
+    } else {
+        req.flash("success", "user is registered successfully ")
+    }
+    
+    res.redirect("/hello");
 })
 
-app.get("/getcookies", (req, res) => {
-    res.cookie("hello", "ronaldo")
-    res.send("the cookie is here ");
+app.get("/hello", (req, res) => {
+    res.render("page.ejs", {name: req.session.name })
 })
 
-app.get("/greet", (req,res) => {
-    let { name = "man" } = req.cookies;
-    res.send(`Hi ${name}`);
-})
+// app.get("/test", (req, res) => {
+//     res.send("test successful");
+// })
 
-app.get("/", (req, res) => {
-    console.dir(req.cookies)
-    res.send("hii i am a root !")
-})
+// app.get("/reqcount", (req, res) => {
+//     if(req.session.count) {
+//         req.session.count++
+//     } else {
+//         req.session.count = 1;
+//     }
 
-
-
-app.get("/", (req, res) => {
-    res.send("i am root ...")
-})
-
-app.use("/users", users);
-app.use("/posts", posts);
+//     res.send(` you sent request ${req.session.count} times `)
+// })
 
 app.listen(3000, () => {
     console.log("server is listening at port 3000")
