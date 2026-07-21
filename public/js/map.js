bc501f7c4d1f63759d9c3f4bd95af057141ab5f5
bhushan-rajput18 <patilbhushan0222@gmail.com>
2026-07-21 18:22:36 +0530

    Map Marker
     adding a marker on map and it goes to the city whatever listing have , I think it does work after editing it doesnt changes we'll see that afterwards

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,13 @@
 
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+    console.log(coordinates);
+
+const marker = new mapboxgl.Marker({color: "red"})
+    .setLngLat(coordinates)
+    .addTo(map);
